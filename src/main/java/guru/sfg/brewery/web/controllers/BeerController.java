72b276ceab532f56b15e0104d842e997642bd09f
pdrr98 <pedro.98.gmr@gmail.com>
2024-04-13 22:10:38 -0600

    Spring security added, initCreationForm in beerController now return beerStyles, createBeer.html now acces beer styles through the model. So template parsing error is now fixed.

--- src/main/java/guru/sfg/brewery/web/controllers/BeerController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/BeerController.java
@@ -20,6 +20,7 @@ package guru.sfg.brewery.web.controllers;
 import guru.sfg.brewery.domain.Beer;
 import guru.sfg.brewery.repositories.BeerInventoryRepository;
 import guru.sfg.brewery.repositories.BeerRepository;
+import guru.sfg.brewery.web.model.BeerStyleEnum;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -85,6 +86,7 @@ public class BeerController {
     @GetMapping("/new")
     public String initCreationForm(Model model) {
         model.addAttribute("beer", Beer.builder().build());
+        model.addAttribute("beerStyles", BeerStyleEnum.values());
         return "beers/createBeer";
     }
 
